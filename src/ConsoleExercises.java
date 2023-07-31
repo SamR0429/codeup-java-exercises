01a399674a9d46aee54b8da634fb7ad697411789
Samantha Ramos <Samanatha.Ramos01@gmail.com>
2023-07-31 17:00:29 -0500

    finished excercise

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.println(pi);
+//
+//        System.out.printf("The value of pi is approximately, %s.", pi);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("enter a number");
+//        int userInput = scanner.nextInt(); // this breaks if you put anything but a integer(whole number) in there
+//        System.out.println("You entered: \"" + userInput + "\" ");
+
+//        Scanner questiontwo = new Scanner(System.in);
+//        System.out.println("enter three words");
+//        String userInput = scanner.next();
+//        String userInput2 = scanner.next();
+//        String userInput3 = scanner.next();
+//        System.out.println("you entered: \"" + userInput + "\" ");
+//        System.out.println("you entered: \"" + userInput2 + "\" ");
+//        System.out.println("you entered: \"" + userInput3 + "\" ");
+
+//        Scanner questionthree = new Scanner(System.in); // this one doesnt spit out the whole sentence inputted just the first letter or word
+//        System.out.println("enter a sentence");
+//        String userInput = questionthree.next();
+//        System.out.println("you entered: \"" + userInput + "\" ");
+
+//        Scanner questionfour = new Scanner(System.in);
+//        System.out.println("enter a sentence");
+//        String userInput = questionfour.nextLine(); // this one does spit out the whole sentence because of the nextLine part
+//        System.out.println("you entered: \"" + userInput + "\" ");
+
+        Scanner questiononeb = new Scanner(System.in);
+        System.out.println("what is the length of the classroom?");
+        String userInput = questiononeb.nextLine();
+        int number1 = Integer.parseInt(userInput);
+        System.out.println("what is the width of the classroom?");
+        String userInput2 = questiononeb.nextLine();
+        int number2 = Integer.parseInt(userInput2);
+        System.out.println("The length is: \"" + number1 + "\" ");
+        System.out.println("The width is: \"" + number2 + "\" ");
+
+        int areaCal = number2 * number1;
+        System.out.println("this is the area: \"" + areaCal + "\" ");
+        int perimeterCal = number2 + number1;
+        System.out.println("this is the area: \"" + perimeterCal + "\" ");
+    }
+
+
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,39 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.print("Hellllooooo, World");
+
+        int myFavoriteNumber = 28;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "my name is sam"; //Strings can be hold the value of numbers and strings w/o the console griping
+        System.out.println(myString);
+
+        float myNumber = 123; // this works w/ long only when there is a L @ the end of the number or when you have it as a float
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++); //this prints out 6 cause it reads it as 5 + 1
+        System.out.println(x);
+
+
+        int y = 5;
+        System.out.println(++y); // even though it looks diff it's the same thing as above
+        System.out.println(y);
+
+
+
+
     }
+
+    byte favNum = 12;
+    boolean whateverIWant = true;
+    String someString = "hello there";
+
+
 }
+
+
