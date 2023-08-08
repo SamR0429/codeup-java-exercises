2b894614dca9b0a7a2d71feea8aa8efdbc0d7f5a
Samantha Ramos <Samanatha.Ramos01@gmail.com>
2023-08-08 09:25:39 -0500

    finsihed exercise

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,10 +1,23 @@
 package shapes;
 
-//public class Square extends Rectangle {
-//
-//
-//    public Square(double side) {
-//        super.length = side;
-//        super.width = side;
-//    }
-//}
+public class Square extends Rectangle {
+
+double side;
+
+    public Square(double side) {
+        super(side, side);
+        this.side = side;
+
+    }
+
+    public double getArea(){
+        return (double) (Math.pow(this.side, 2));
+    }
+
+    public double getPerimeter(){
+        return (4 * this.side);
+    }
+
+
+
+}
