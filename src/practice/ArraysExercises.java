6a365cf575ee09170f94781e3c83eea8ea9539cf
Samantha Ramos <Samanatha.ramos01@gmail.com>
2023-08-13 21:18:58 -0500

    practicing java

---
+++ src/practice/ArraysExercises.java
@@ -0,0 +1,41 @@
+package practice;
+
+public class ArraysExercises {
+
+    public static void main(String[] args) {
+
+        //1.
+//        int [] numberArray = {12, 46, 78, 34, 80};
+//        String [] stringArray = {"sam", "dette", "seth", "meemaw"};
+
+        //2.
+//        int sumArray[] = {34, 78, 37, 90, 67};
+
+        //3.
+        String[] patternArray = {"- - - - - - - - - -"};
+
+//////////////////////////////////////////////////////////////////
+//1.
+//        Arrays.sort(numberArray);
+//        System.out.println("this is the sorted numbers array: " + Arrays.toString(numberArray));
+//
+//        Arrays.sort(stringArray);
+//        System.out.println("this is the sorted string: " + Arrays.toString(stringArray));
+
+//2.
+//        int sum = 0;
+//
+//        for (int i : sumArray) {
+//            sum += i;
+//            System.out.println("the sum is: " + sum);
+//        }
+
+        //3.
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(patternArray[i]);
+//        }
+
+
+
+    }
+}
