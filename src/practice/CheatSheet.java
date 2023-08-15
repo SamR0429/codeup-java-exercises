09ff703bd44dec3e76b22c71d82ad4e06e5f8b30
Samantha Ramos <Samanatha.ramos01@gmail.com>
2023-08-14 20:54:49 -0500

    working on test

---
+++ src/practice/CheatSheet.java
@@ -0,0 +1,28 @@
+package practice;
+
+public class CheatSheet {
+
+    //instance variables
+    int var1;
+    double var1b;
+    String var2;
+    boolean var3;
+
+    //methods
+    public static void method1(/*parameter(s)*/) {
+        System.out.println("hi");
+    }
+
+    public static int method2() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        //to declare and initialize objects
+//        classname nameOfObject = new classname();
+
+
+    }
+
+}
